35e9564b9dc345db4b5c47f43df056f05a22e302
Stephen Chung <spchung@andrew.cmu.edu>
2018-03-07 14:00:14 -0500

    modify example

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
 from office import OfficeQuotes
 
 season3 = OfficeQuotes(3)
-print season3.random_quote()
+random_quote = season3.random_quote()
+random_quote_from_episode7 = season3.random_quote(7)
+quotes_by_kevin = season3.quotes_from_episode(5)['kevin']
 
-
-
+print random_quote
+print random_quote_from_episode7
+print quotes_by_kevin

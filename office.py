1e2e011fa84855ee3ea8bbd70c0fce2f0be05b13
Stephen Chung <spchung@andrew.cmu.edu>
2018-09-20 20:56:32 -0700

    cleanup

--- office.py
+++ office.py
@@ -3,43 +3,52 @@ import random
 from bs4 import BeautifulSoup
 
 class OfficeQuotes(object):
-    base_url = "http://www.officequotes.net"
+    base_url = 'http://www.officequotes.net'
 
     def __init__(self, season):
         self.season = season
         self.quotes = {}
     
     def quotes_from_episode(self, episode):
-        if episode in self.quotes: return self.quotes[episode]
+        if episode in self.quotes:
+            return self.quotes[episode]
 
         soup = BeautifulSoup(self.get_html(episode), 'html.parser')
-        quote_divs = soup.find_all("div", class_="quote")
+        quote_divs = soup.find_all('div', class_='quote')
 
         lines = {}
         for block in quote_divs:
             # https://stackoverflow.com/a/9942822/8109239
             text = block.text.encode('utf-8').strip()
             for line in text.split("\n"):
-                if ":" not in line: continue
-                character, words = line.split(":")[0].lower(), line.split(":")[1]
+                if ':' not in line:
+                    continue
+
+                character, words = line.split(':')[0].lower(), line.split(':')[1]
 
                 # fix typos
-                if character == "michel": character = "michael"
+                if character == 'michel':
+                    character = "michael"
 
                 # encoding corrections
-                words = words.replace("\xe2\x80\x94", "-")
-                words = words.replace("\xe2\x80\x99", "'")
-                words = words.replace("\xe2\x80\x98", "'")
+                words = words.replace('\xe2\x80\x94', '-')
+                words = words.replace('\xe2\x80\x99', "'")
+                words = words.replace('\xe2\x80\x98', "'")
                 words = words.replace("\'", "'")
                 words = words.strip()
 
                 if character not in lines:
                     lines[character] = []
+
                 # TODO: split lines by individual sentences
                 if len(words) > 0: lines[character].append(words)
 
         self.quotes[episode] = lines
         return lines
+
+    def quotes_from_character(self, episode, character):
+        quotes = self.quotes_from_episode(episode)
+        return quotes[character]
 
     def random_quote(self, episode=None):
         if episode is None:
@@ -47,17 +56,20 @@ class OfficeQuotes(object):
         episode_quotes = self.quotes_from_episode(episode)
         character = random.choice(episode_quotes.keys())
         line = random.choice(episode_quotes[character])
-        if "'" in line: print line
         return character, line
 
     def get_html(self, episode):
         headers = {
-            "Accept-Charset" : "utf-8"
+            'Accept-Charset' : 'utf-8'
         }
-        url = "%s/no%d-%02d.php" % (OfficeQuotes.base_url, self.season, episode)
+        url = '{base_url}/no{season}-{episode}.php'.format(
+            base_url=OfficeQuotes.base_url,
+            season=self.season,
+            episode=('%02d' % episode)
+        )
         res = requests.get(url, headers=headers)
 
         return res.text if res.status_code == 200 else ""
 
     def __repr__(self):
-        return "Quotes from \"The Office\", Season %d" % self.season
+        return 'Quotes from "The Office", Season {}'.format(self.season)
